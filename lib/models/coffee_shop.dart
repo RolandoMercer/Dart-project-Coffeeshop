ef7a66ae7dbd87dfb814296d9a314f255ad1b8bb
Rolando Roberts <rolandoalvin111@gmail.com>
2024-02-21 10:44:02 -0400

    my first commit

---
+++ lib/models/coffee_shop.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'coffee.dart';
+
+class CoffeeShop extends ChangeNotifier {
+
+  final List<Coffee> _shop =[
+    
+    Coffee(
+      name: 'Long Black',
+      price: "4.10",
+      imagePath: "lib/images/black.png",
+    ),
+
+     Coffee(
+      name: 'Latte',
+      price: "4.20",
+      imagePath: "lib/images/latte.png",
+    ),
+
+     Coffee(
+      name: 'Expresso',
+      price: "3.50",
+      imagePath: "lib/images/expresso.png",
+    ),
+
+     Coffee(
+      name: 'Iced Coffee',
+      price: "4.40",
+      imagePath: "lib/images/iced-coffee.png",
+    ),
+  ];
+
+  List<Coffee> _userCart = [];
+
+  List<Coffee> get coffeeShop => _shop;
+
+  List<Coffee> get userCart => _userCart;
+
+  void addItemToCart(Coffee coffee) {
+    _userCart.add(coffee);
+    notifyListeners();
+  }
+
+  void removeItemFromCart(Coffee coffee) {
+    _userCart.remove(coffee);
+    notifyListeners();
+  }
+
+}
